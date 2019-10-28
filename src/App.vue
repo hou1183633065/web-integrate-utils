09bfbf60ec2a74c5cfbbea4458583a5ddf2f5b7e
houzhiqiang <zhq.hou@muheda.com>
2019-10-28 19:13:38 +0800

    添加es6跟async/await编译

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,25 @@
 <script>
 // import utils from "./index";
 export default {
-  name: 'app',
+  name: "app",
   mounted() {
-    console.log(utils);
+    this.$nextTick(() => {
+      console.log(utils);
+      this.initMap();
+    });
     // console.log(utils.url.getFormatUrl('https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=vue%20init%20webpack-simple%20my-project&oq=webpack%25E5%25BC%2580%25E5%258F%2591%25E6%25A8%25A1%25E6%259D%25BF&rsv_pq=f27f4cc90001c4f6&rsv_t=1ddcgy0yIJPMDZR2i5wmViDH1cmEyacTEFHrRx2GsTVfAZkR0jZ79Sogj%2Fg&rqlang=cn&rsv_enter=1&rsv_dl=tb&inputT=656&rsv_sug3=130&rsv_sug1=63&rsv_sug7=000&rsv_sug2=0&rsv_sug4=657&rsv_sug=1'));
-    
   },
-}
+  methods: {
+    async initMap() {
+      console.log(utils);
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,7 +32,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
